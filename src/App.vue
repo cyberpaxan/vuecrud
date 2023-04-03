8e116499d6b45ee4048833954a94a1164bb9cd8c
cyberpaxan <igorl098765@gmail.com>
2023-04-03 23:28:41 +0500

    gsdf

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,46 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <div class="app">
+        <post-form @create="createPost" />
+        <post-list :posts="posts" />
+    </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import PostForm from '@/components/PostForm.vue';
+import PostList from '@/components/PostList.vue';
+
+export default {
+    components: {
+        PostList,
+        PostForm,
+    },
+
+    data() {
+        return {
+            posts: [
+                { id: 1, title: 'Описание поста 1', body: 'Описание поста 1' },
+                { id: 2, title: 'Описание поста 2', body: 'Описание поста 2' },
+                { id: 3, title: 'Описание поста 3', body: 'Описание поста 3' },
+            ],
+        };
+    },
+
+    methods: {
+        createPost(post) {
+            this.posts.push(post);
+        },
+    },
+};
+</script>
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.app {
+    padding: 20px;
 }
 </style>
---
+++ src/components/UI/MyButton.vue
@@ -0,0 +1,14 @@
+<template>
+    <button class="btn"><slot></slot></button>
+</template>
+
+<script></script>
+
+<style scoped>
+.btn {
+    padding: 10px 15px;
+    color: teal;
+    border: 1px solid teal;
+    background: none;
+}
+</style>
